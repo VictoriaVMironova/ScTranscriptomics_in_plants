b3af6550a32d6e4800fdc9c00c4f5509dbe7ef3d
VictoriaVMironova <victoria.mironova@ru.nl>
2023-03-20 17:41:29 +0100

    CP3 practice updated

--- CP3_from_clusters_to_marker_genes.R
+++ CP3_from_clusters_to_marker_genes.R
@@ -10,9 +10,11 @@ library(tidyverse)
 #To make your data calculation reproducible between independent runs, set the seed for the random Number generator
 set.seed(42)
 
-#Task 1: Adjusting the number of clusters
-#lets repeat the dataset preprocessing from the beginning
-path <- 'E:/R_images/Leaf_LopezAnido_DevCell/Data/GSM5097888_Leaf-HVYNNBBXX/filtered_gene_bc_matrices/Arabidopsis/'
+#Lets reuse the Seurat object we have created in computer practice 2.
+leaf.dataset <- readRDS('Data/leaf.dataset.rds')
+
+#Or you can create the object again using the summary script below
+path <- 'Data/GSM5097889/'
 leaf.dataset<-Read10X(path, gene.column = 1)
 leaf.dataset <- CreateSeuratObject(counts = leaf.dataset, project = "leaf")
 leaf.dataset[["percent.mt"]] <- PercentageFeatureSet(leaf.dataset, pattern = "^ATM")
@@ -22,34 +24,33 @@ leaf.dataset <- SCTransform(leaf.dataset)
 leaf.dataset <- RunPCA(leaf.dataset,verbose = FALSE)
 leaf.dataset <- RunUMAP(leaf.dataset, dims = 1:50, verbose = FALSE)
 leaf.dataset <- FindNeighbors(leaf.dataset, dims = 1:50, verbose = FALSE)
+leaf.dataset <- FindClusters(leaf.dataset, resolution = 1, verbose = FALSE)
 
-#but this time change resolution parameter in FindClusters function
-leaf.dataset_res1 <- FindClusters(leaf.dataset, resolution = 1, verbose = FALSE)
-leaf.dataset_res0.2 <- FindClusters(leaf.dataset, resolution = 0.2, verbose = FALSE)
-leaf.dataset_res1.5 <- FindClusters(leaf.dataset, resolution = 1.5, verbose = FALSE)
-
+#The cell populations:
 DimPlot(leaf.dataset, label = TRUE, pt.size = 1.5, label.size = 10) + NoLegend()
-DimPlot(leaf.dataset_res0.2, label = TRUE, pt.size = 1.5, label.size = 10) + NoLegend()
-DimPlot(leaf.dataset_res1.5, label = TRUE, pt.size = 1.5, label.size = 10) + NoLegend()  
 
-#Task 2: Finding marker genes
-leaf0.2.markers <- FindAllMarkers(leaf.dataset_res0.2, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+#Aim 1: Finding marker genes
+leaf.markers <- FindAllMarkers(leaf.dataset, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
 
-write.csv(leaf0.2.markers, file = "Data/leaf_res0.2_markers.csv")
-#leaf0.2.markers<- read.csv(file = "Data/leaf_res0.2_markers.csv", header = TRUE)  
+write.csv(leaf.markers, file = "Data/leaf_markers.csv")
+#leaf.markers<- read.csv(file = "Data/leaf_markers.csv", header = TRUE)  
 
 #visualizing marker genes
-top2<-leaf0.2.markers %>%
+top2<-leaf.markers %>%
         group_by(cluster) %>%
         slice_max(n = 2, order_by = avg_log2FC)
 
-DoHeatmap(leaf.dataset_res0.2, features = top2$gene) + NoLegend()
+top2
 
-VlnPlot(leaf.dataset_res0.2, features = top2$gene, pt.size = 0.2, ncol = 7)
+DoHeatmap(leaf.dataset, features = top2$gene) + NoLegend()
 
-#Differential expressed genes between two clusters
+VlnPlot(leaf.dataset, features = top2$gene, pt.size = 0.2, ncol = 7)
 
-cluster2.markers <- FindMarkers(leaf.dataset_res1, ident.1 = 2, ident.2 = 1, min.pct = 0.25)
+DotPlot(leaf.dataset, features = unique(top2$gene), cols = c("blue", "red"), dot.scale = 8)+ coord_flip()
+
+#Aim 2: Differentially expressed genes between two clusters
+
+cluster2.markers <- FindMarkers(leaf.dataset, ident.1 = 2, ident.2 = 1, min.pct = 0.25)
 head(cluster2.markers, n = 8)
 
 top10 <- cluster2.markers %>% 
@@ -57,8 +58,21 @@ top10 <- cluster2.markers %>%
             row.names()
 VlnPlot(leaf.dataset_res1, features = top10, pt.size = 0.2, ncol = 5)
 
+#FindMarkers(leaf.dataset, ident.1 = 2, ident.2 = c(1,6), min.pct = 0.25)
+#FindMarkers(leaf.dataset, ident.1 = 2, ident.2 = c(1,6), min.pct = 0.25, test.use = "roc")
 
-#Task 1:Build a heatmap for TOP5 marker genes
 
+#Aim 3: Annotate clusters.
+marker_genes<- c('AT1G11850', 'AT3G24140', 'AT2G05100', 'AT5G01530', 'AT4G10340', 'AT1G67090', 'AT5G38410', 'AT3G54890', 'AT1G29920', 'AT3G08940', 'AT3G27690', 'AT5G38430', 'AT1G76570', 'AT4G21750', 'AT2G26330', 'AT3G45640', 'AT2G42840', 'AT5G53210', 'AT5G60880', 'AT2G26330', 'AT1G80080', 'AT1G34245', 'AT3G26744', 'AT1G12860', 'AT2G42840', 'AT5G53210', 'AT5G60880', 'AT2G26330', 'AT5G62230', 'AT5G07180', 'AT1G80080', 'AT1G34245', 'AT1G04110', 'AT4G31805', 'AT3G26744', 'AT1G12860', 'AT3G45640', 'AT3G06120', 'AT5G53210', 'AT2G26330', 'AT5G62230', 'AT5G07180', 'AT1G80080', 'AT1G34245', 'AT2G20875', 'AT1G04110', 'AT4G31805', 'AT3G26744', 'AT1G12860', 'AT3G45640', 'AT3G06120', 'AT2G26330', 'AT5G62230', 'AT5G07180', 'AT2G20875', 'AT1G04110', 'AT4G31805', 'AT3G26744', 'AT1G12860', 'AT1G14350', 'AT3G45640', 'AT3G24140', 'AT3G06120', 'AT2G26330', 'AT5G62230', 'AT5G07180', 'AT2G20875', 'AT1G04110', 'AT3G26744', 'AT1G12860', 'AT1G14350', 'AT3G45640', 'AT3G24140', 'AT2G26330', 'AT5G62230', 'AT5G07180', 'AT1G04110', 'AT1G14350', 'AT5G46240', 'AT3G45640', 'AT3G17300', 'AT5G46240', 'AT2G26330', 'AT3G45640', 'AT4G12970', 'AT2G17950', 'AT2G27250', 'AT2G45190', 'AT2G27250', 'AT4G17710', 'AT1G55580', 'AT2G22850', 'AT3G28730', 'AT4G32880', 'AT5G16560')
 
+DoHeatmap(leaf.dataset, features = marker_genes) + NoLegend() 
+
+leaf.dataset <- RenameIdents(leaf.dataset,
+                             `1`="Mesophyll",
+                             `2`='Mesophyll')
+leaf.dataset$CellTypes<-Idents(leaf.dataset)
+
+DoHeatmap(leaf.dataset, features = marker_genes) + NoLegend() 
+
+#Your turn...
 
